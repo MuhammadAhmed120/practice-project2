68b0ca31727c022d50c243b8363efc51ecc666ae
MuhammadAhmed120 <muhammadahmed120192@gmail.com>
2023-05-27 00:41:02 +0500

    color change function

--- app.js
+++ app.js
@@ -14,6 +14,44 @@ function addPost(){
             <p class="posted-content">${postValue.value}</p>  
         </li>
     `
+
+    
     postDiv.innerHTML += post;
     postValue.value = "";
+}
+// postValue.style.color = "white";
+
+var blackColor = false;
+function colorChange(colorpick) {
+    if (colorpick === "black") {
+        postValue.style.backgroundColor = "black";
+        postValue.style.color = "white";
+    } else if (colorpick === "pink") {
+        postValue.style.backgroundColor = "pink";
+        postValue.style.color = "white";
+    } else if (colorpick === "orange") {
+        postValue.style.backgroundColor = "orange";
+        postValue.style.color = "black";
+    } else if (colorpick === "brown") {
+        postValue.style.backgroundColor = "brown";
+        postValue.style.color = "white";
+    } else if (colorpick === "blue") {
+        postValue.style.backgroundColor = "blue";
+        postValue.style.color = "white";
+    } else if (colorpick === "yellow") {
+        postValue.style.backgroundColor = "yellow";
+        postValue.style.color = "black";
+    } else if (colorpick === "purple") {
+        postValue.style.backgroundColor = "purple";
+        postValue.style.color = "white";
+    } else if (colorpick === "green") {
+        postValue.style.backgroundColor = "green";
+        postValue.style.color = "white";
+    } else if (colorpick === "red") {
+        postValue.style.backgroundColor = "red";
+        postValue.style.color = "white";
+    } else if (colorpick === "gray") {
+        postValue.style.backgroundColor = "gray";
+        postValue.style.color = "white";
+    }
 }
